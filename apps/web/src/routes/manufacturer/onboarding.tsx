8aabc2a6ebe544c1e499d15864f050329633dd03
paulods0 <pauloluguenda0@gmail.com>
2026-01-24 20:25:11 +0100

    second commit

--- apps/web/src/routes/manufacturer/onboarding.tsx
+++ apps/web/src/routes/manufacturer/onboarding.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@/components/ui/button';
 import { createFileRoute, Link } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/manufacturer/onboarding')({
   component: RouteComponent,
